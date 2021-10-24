d77037899428a820336596dc14706d743938e11c
ennnm <jiaen.1sc4@gmail.com>
2021-10-24 18:25:48 +0800

    wip pixel moving around grid

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,29 @@ import React from 'react';
 import GridElem from './components/grid.jsx';
 
 export default function App() {
+  const handleKeyDown = (e) => {
+    console.log('e.code :>> ', e.code);
+    //TODO FIGURE OUT HOW TO LOG KEYBOARD EVENTS
+    //  switch (e.code) {
+    //    case 'keyA':
+    //      movePlayer(-1, 0);
+    //      break;
+    //    case 'keyW':
+    //      movePlayer(0, 1);
+    //      break;
+    //    case 'keyD':
+    //      movePlayer(1, 0);
+    //      break;
+    //    case 'keyS':
+    //      movePlayer(0, -1);
+
+    //      break;
+    //  }
+  };
+
   return (
-    <div>
-     <GridElem/>
+    <div onKeyDown={handleKeyDown}>
+      <GridElem />
     </div>
   );
 }
--- src/components/grid.jsx
+++ src/components/grid.jsx
@@ -2,7 +2,9 @@ import React, { useState } from 'react';
 
 const numCols = 30;
 const numRows = 20;
+const playerPosition = [getRandomInt(numCols), getRandomInt(numRows)]; //X,Y
 
+console.log('playerPosition :>> ', playerPosition);
 function getRandomColor() {
   var letters = '0123456789ABCDEF';
   var color = '#';
@@ -11,13 +13,15 @@ function getRandomColor() {
   }
   return color;
 }
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
 const userColor = getRandomColor();
 
-const genGridArray = () => {
+const genGridArray = (fill = null) => {
   //TODO: fine tune sizing, size for tablet
-
   //nested arrays easier for checking adjacency vs using flat array
-  const arr = [...Array(numRows)].map(() => Array(numCols).fill(''));
+  const arr = [...Array(numRows)].map(() => Array(numCols).fill(fill));
   return arr;
 };
 const clickOnCell = (index) => {
@@ -43,7 +47,9 @@ const gridCells = (arr2d) => {
 
 //click grid for special objects
 const clickGrid = (arr2d) => {
+  console.log('arr2d :>> ', arr2d);
   const arr1d = [].concat(...arr2d);
+  console.log('arr1d :>> ', arr1d);
   const cells = arr1d.map((cell, index) =>
     cell !== null ? (
       <button
@@ -54,7 +60,6 @@ const clickGrid = (arr2d) => {
         }}
         className="cell"
         id={`row${Math.floor(index / numCols)}_col${index % numCols}`}
-        // style={{ backgroundColor: userColor }}
       ></button>
     ) : (
       <div
@@ -70,15 +75,15 @@ const clickGrid = (arr2d) => {
 //players grid
 const playersGrid = (playersArr2d) => {
   //implant player in
-  playersArr2d[0][0] = userColor;
-  console.log('playersArr2d[0][0] :>> ', playersArr2d[0][0]);
+  playersArr2d[playerPosition[1]][playerPosition[0]] = userColor;
+  // playersArr2d[0][0] = userColor;
   const arr1d = [].concat(...playersArr2d);
   //read arr2d for wall grid, set div style accordigng to color stored in array
   const cells = arr1d.map((cell, index) => (
     <div
       className="cell"
       id={`player${Math.floor(index / numCols)}${index % numCols}`}
-      style={{ backgroundColor: { cell } }}
+      style={{ backgroundColor: cell }}
     ></div>
   ));
   return cells;
@@ -91,7 +96,7 @@ const baseGrid = (arr2d) => {
     <div
       className="cell"
       id={`base${Math.floor(index / numCols)}${index % numCols}`}
-      // style={{ backgroundColor: userColor }}
+      style={{ backgroundColor: cell }}
     ></div>
   ));
   return cells;
@@ -102,10 +107,55 @@ const addUser = (rowCoord, colCoord, arr) => {};
 //MOVE DOT IN GRID
 
 export default function GridElem() {
+  const [userPosition, setUserPosition] = useState([
+    getRandomInt(numCols),
+    getRandomInt(numRows),
+  ]);
   const clickCells = clickGrid(genGridArray());
   const playerCells = playersGrid(genGridArray());
-  const baseCells = baseGrid(genGridArray());
+  const baseCells = baseGrid(genGridArray('rgb(255, 255, 255)'));
   //fill in with row and cell elements
+
+  const movePlayer = (x, y) => {
+    let destinationX = userPosition[0];
+    let destinationY = userPosition[1];
+    console.log('destinationX :>> ', destinationX);
+    console.log('destinationY :>> ', destinationY);
+    if (destinationX + x >= 0 && destinationX + x < numCols) {
+      destinationX += x;
+    }
+    if (destinationY + y >= 0 && destinationY + y < numRows) {
+      destinationY += y;
+    }
+    console.log('destinationX :>> ', destinationX);
+    console.log('destinationY :>> ', destinationY);
+    setUserPosition([destinationX, destinationY]);
+
+    //TODO FIGURE HOW TO GET THIS TO WORK
+  };
+
+  const handleKeyDown = (e) => {
+    console.log('e.code :>> ', e.code);
+    console.log('userPosition :>> ', userPosition);
+    switch (e.code) {
+      case 'keyA':
+        movePlayer(-1, 0);
+        break;
+      case 'keyW':
+        movePlayer(0, 1);
+        break;
+      case 'keyD':
+        movePlayer(1, 0);
+        break;
+      case 'keyS':
+        movePlayer(0, -1);
+
+        break;
+    }
+    console.log('userPosition2 :>> ', userPosition);
+  };
+  document.addEventListener('keydown', handleKeyDown);
+
   return (
     <div position-relative>
       <div id="baseGrid" className="grid-container position-absolute">
