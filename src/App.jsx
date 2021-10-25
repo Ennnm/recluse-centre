a1c344bf23929cf9a97d8085c8708a18f6df342a
Lee Chuan Xin <chuanxin@gametize.com>
2021-10-26 03:43:24 +0800

    fix: cleaned up routing components.

--- src/App.jsx
+++ src/App.jsx
@@ -6,37 +6,19 @@ import {
 } from 'react-router-dom';
 
 // auth pages
-import LoginPage from './components/Login/LoginPage.jsx';
-import RegisterPage from './components/Register/RegisterPage.jsx';
+import Login from './components/Login/LoginPage.jsx';
+import Register from './components/Register/RegisterPage.jsx';
 // main pages
 import GridElem from './components/grid.jsx';
 // error pages
-import Error404Page from './components/Error/Error404Page.jsx';
+import Error404 from './components/Error/Error404Page.jsx';
 
 function Grid() {
   return (
-    <div>
+    <>
       <h1>Hello Jia En?</h1>
       <GridElem />
-    </div>
-  );
-}
-
-function Login() {
-  return (
-    <LoginPage />
-  );
-}
-
-function Register() {
-  return (
-    <RegisterPage />
-  );
-}
-
-function Error404() {
-  return (
-    <Error404Page />
+    </>
   );
 }
 
