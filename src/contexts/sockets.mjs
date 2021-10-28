1b53317bb589fab3fcb2b2420bba89286b6e2393
ennnm <jiaen.1sc4@gmail.com>
2021-10-28 17:55:20 +0800

    removed a comment

--- src/contexts/sockets.mjs
+++ src/contexts/sockets.mjs
@@ -2,5 +2,4 @@ import { io } from 'socket.io-client';
 import React from 'react';
 
 export const socket = io();
-// export const socket = io('http://localhost:3004');
 export const SocketContext = React.createContext();
