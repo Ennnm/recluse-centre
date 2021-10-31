ca53541a36aeba83b594feeb6bc6f94344999186
Lee Chuan Xin <chuanxin@gametize.com>
2021-11-01 04:54:11 +0800

    feat: pedrotech chat ui.

--- src/components/Chat/Chat.jsx
+++ src/components/Chat/Chat.jsx
@@ -1,12 +1,17 @@
 import React, { useState, useEffect } from 'react';
+import ScrollToBottom from 'react-scroll-to-bottom';
 
 // eslint-disable-next-line react/prop-types
 export default function Chat({ socket, username, room }) {
   const [currentMessage, setCurrentMessage] = useState('');
+  const [messageList, setMessageList] = useState([]);
 
   useEffect(() => {
     // eslint-disable-next-line react/prop-types
     socket.on('chat:receive', (data) => {
+      // set message list when RECEIVING a message
+      setMessageList((list) => [...list, data]);
+      console.log('message received!');
       console.log(data);
     });
   }, [socket]);
@@ -22,6 +27,9 @@ export default function Chat({ socket, username, room }) {
 
       // eslint-disable-next-line react/prop-types
       await socket.emit('chat:send', messageData);
+      // set message list also when we SEND / EMIT our own message
+      setMessageList((list) => [...list, messageData]);
+      setCurrentMessage('');
     }
   };
 
@@ -29,14 +37,38 @@ export default function Chat({ socket, username, room }) {
     setCurrentMessage(event.target.value);
   };
 
+  const handleCurrentMessageKeyPress = (event) => {
+    if (event.key === 'Enter') {
+      sendMessage();
+    }
+  };
+
   return (
     <div className="chat-window">
       <div className="chat-header">
         <p>Live Chat</p>
       </div>
-      <div className="chat-body" />
+      <div className="chat-body">
+        <ScrollToBottom className="message-container">
+          {messageList.map((messageContent) => (
+            <div className="message" id={(username === messageContent.author ? 'you' : 'other')}>
+              <div>
+                <div className="message-content">
+                  <p>
+                    {messageContent.message}
+                  </p>
+                </div>
+                <div className="message-meta">
+                  <p id="time">{messageContent.time}</p>
+                  <p id="author">{messageContent.author}</p>
+                </div>
+              </div>
+            </div>
+          ))}
+        </ScrollToBottom>
+      </div>
       <div className="chat-footer">
-        <input type="text" placeholder="Hey..." value={currentMessage} onChange={handleCurrentMessageChange} />
+        <input type="text" placeholder="Hey..." value={currentMessage} onChange={handleCurrentMessageChange} onKeyPress={handleCurrentMessageKeyPress} />
         <button type="button" onClick={sendMessage}>
           &#9658;
         </button>
