5f49deda7f6647adacea4bf6eba5d27f4465cb73
ennnm <jiaen.1sc4@gmail.com>
2021-11-04 10:36:45 +0800

    tools popup with icons

--- src/components/World/UserModal.jsx
+++ src/components/World/UserModal.jsx
@@ -2,14 +2,72 @@ import React from 'react';
 import { Card } from 'react-bootstrap';
 import '../../styles.scss';
 import 'tailwindcss/tailwind.css';
-// src / components / World / UserModal.jsx;
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+import {
+  faFillDrip,
+  faChessRook,
+  faStickyNote,
+  faThLarge,
+  faIcons,
+} from '@fortawesome/free-solid-svg-icons';
+
+const WallTool = () => (
+  <div>
+    <button className="h-12 w-12 border-1 m-2">
+      <FontAwesomeIcon
+        className="text-3xl m-2 text-purple-700"
+        icon={faChessRook}
+      />
+    </button>
+    Wall
+  </div>
+);
+const RoomTool = () => (
+  <div>
+    <button className="h-12 w-12 border-1 m-2">
+      <FontAwesomeIcon
+        className="text-3xl  m-2 text-red-200"
+        icon={faThLarge}
+      />
+    </button>
+    Room
+  </div>
+);
+const CharFiller = () => (
+  <div>
+    <button className="h-12 w-12 border-1 m-2">
+      <FontAwesomeIcon
+        className="text-3xl  m-2 text-green-700"
+        w
+        icon={faIcons}
+      />
+    </button>
+    Char
+  </div>
+);
+const WidgetTool = () => (
+  <div>
+    <button className="h-12 w-12 border-1 m-2 ">
+      <FontAwesomeIcon
+        className="text-3xl m-2 text-yellow-400"
+        icon={faStickyNote}
+      />
+    </button>
+    Widget
+  </div>
+);
 const ToolsModal = () => {
   const placement = 'top';
-
   return (
-    <Card className="z-10">
-      <Card.Body>This is some text within a card body.</Card.Body>
-    </Card>
+    <div className="p-4 max-w-sm mx-auto bg-white rounded-xl shadow-md  items-center space-x-4z-10">
+      <div className="flex">
+        <WallTool />
+        <RoomTool />
+        <CharFiller />
+        <WidgetTool />
+      </div>
+    </div>
   );
 };
 
@@ -17,7 +75,14 @@ export default function UserModal(userSquare) {
   const lalala = 5;
   console.log('userSquare in tools :>> ', userSquare);
   return (
-    <div style={{ display: 'none', position: 'absolute' }}>
+    <div
+      className="translate-y-16"
+      style={{
+        display: 'none',
+        position: 'absolute',
+        top: '-300%',
+      }}
+    >
       <ToolsModal />
     </div>
   );
