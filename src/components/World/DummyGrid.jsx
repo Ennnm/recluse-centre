cae97d778478b152124903d013567e0ab7759789
ennnm <jiaen.1sc4@gmail.com>
2021-11-04 23:01:53 +0800

    replaced placeholder baseGrid with dummyGrid

---
+++ src/components/World/DummyGrid.jsx
@@ -0,0 +1,24 @@
+/* eslint-disable react/prop-types */
+import React from 'react';
+import { numRows, numCols } from './GridConstants.mjs';
+
+export default function DummyGrid({}) {
+  const arr1d = [];
+  // read arr2d for wall grid, set div style accordigng to color stored in array
+  const totalNumCells = numRows * numCols;
+
+  for (let i = 0; i < totalNumCells; i += 1) {
+    arr1d.push(
+      <div className="cell gridBorder" key={`dummy${i.toString()}`} />
+    );
+  }
+  return (
+    <div
+      id="baseGrid"
+      className={`grid-container invisible pb-for-chat
+      `}
+    >
+      {arr1d}
+    </div>
+  );
+}
