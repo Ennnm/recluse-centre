c5be2af44aba9c2a9894fc9f02c163ab2c6a3080
Lee Chuan Xin <chuanxin@gametize.com>
2021-11-07 19:27:55 +0800

    Revert "chore: moved imports back to styles.scss."
    This reverts commit ccd1fcadecdf17853558740bf412cc43e53fda59.

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
+import 'bootstrap/scss/bootstrap.scss';
+import 'tailwindcss/tailwind.css';
 import './styles.scss';
 
 import App from './App.jsx';
