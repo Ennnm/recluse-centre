00043d6abdec0ce370455b9b14f90f55e93aca7d
Lee Chuan Xin <chuanxin@gametize.com>
2021-11-07 21:22:20 +0800

    fix: error page styling.

--- src/components/Error/Error404Page.jsx
+++ src/components/Error/Error404Page.jsx
@@ -3,7 +3,10 @@ import React from 'react';
 export default function Error404Page() {
   return (
     <div className="pt-5">
-      <h1>404! Oh my god!</h1>
+      <div className="pt-3">
+        <h1 className="text-5xl text-center text-white">404! Oh my god!</h1>
+      </div>
+
     </div>
   );
 }
